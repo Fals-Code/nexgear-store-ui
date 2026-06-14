3ad39c8913c9eb039f7e85ce58eff03d9107506e
Ahmad Mathlaul Falah <ahmadmathlaulfalah14@gmail.com>
2026-06-14 18:14:18 +0700

    Make About slider move left and right by direction

--- scripts/about-slider.js
+++ scripts/about-slider.js
@@ -15,23 +15,70 @@
   let index = 0;
   let timer = null;
   const interval = 6500;
+  const transitionMs = 900;
+  const directionClasses = [
+    "is-entering-left",
+    "is-entering-right",
+    "is-exiting-left",
+    "is-exiting-right",
+  ];
 
-  function showSlide(nextIndex) {
-    index = (nextIndex + total) % total;
+  function cleanMotionClasses(elements) {
+    elements.forEach((element) => element.classList.remove(...directionClasses));
+  }
 
-    images.forEach((image, i) => {
-      image.classList.toggle("is-active", i === index);
-    });
-
-    texts.forEach((text, i) => {
-      text.classList.toggle("is-active", i === index);
-    });
-
+  function setDots() {
     dots.forEach((dot, i) => {
       const active = i === index;
       dot.classList.toggle("is-active", active);
       dot.setAttribute("aria-selected", active ? "true" : "false");
     });
+  }
+
+  function showSlide(nextIndex, direction = "next", animate = true) {
+    const target = (nextIndex + total) % total;
+    if (target === index && animate) return;
+
+    const currentIndex = index;
+    const currentImage = images[currentIndex];
+    const currentText = texts[currentIndex];
+    const targetImage = images[target];
+    const targetText = texts[target];
+    const movingNext = direction !== "prev";
+
+    cleanMotionClasses(images);
+    cleanMotionClasses(texts);
+
+    if (!animate) {
+      images.forEach((image, i) => image.classList.toggle("is-active", i === target));
+      texts.forEach((text, i) => text.classList.toggle("is-active", i === target));
+      index = target;
+      setDots();
+      return;
+    }
+
+    currentImage?.classList.remove("is-active");
+    currentText?.classList.remove("is-active");
+    currentImage?.classList.add(movingNext ? "is-exiting-left" : "is-exiting-right");
+    currentText?.classList.add(movingNext ? "is-exiting-left" : "is-exiting-right");
+
+    targetImage?.classList.add(movingNext ? "is-entering-right" : "is-entering-left");
+    targetText?.classList.add(movingNext ? "is-entering-right" : "is-entering-left");
+
+    window.requestAnimationFrame(() => {
+      targetImage?.classList.add("is-active");
+      targetText?.classList.add("is-active");
+      targetImage?.classList.remove(movingNext ? "is-entering-right" : "is-entering-left");
+      targetText?.classList.remove(movingNext ? "is-entering-right" : "is-entering-left");
+    });
+
+    index = target;
+    setDots();
+
+    window.setTimeout(() => {
+      currentImage?.classList.remove("is-exiting-left", "is-exiting-right");
+      currentText?.classList.remove("is-exiting-left", "is-exiting-right");
+    }, transitionMs);
   }
 
   function stopAuto() {
@@ -41,22 +88,23 @@
 
   function startAuto() {
     stopAuto();
-    timer = window.setInterval(() => showSlide(index + 1), interval);
+    timer = window.setInterval(() => showSlide(index + 1, "next"), interval);
   }
 
   prev?.addEventListener("click", () => {
-    showSlide(index - 1);
+    showSlide(index - 1, "prev");
     startAuto();
   });
 
   next?.addEventListener("click", () => {
-    showSlide(index + 1);
+    showSlide(index + 1, "next");
     startAuto();
   });
 
   dots.forEach((dot, i) => {
     dot.addEventListener("click", () => {
-      showSlide(i);
+      const direction = i < index ? "prev" : "next";
+      showSlide(i, direction);
       startAuto();
     });
   });
@@ -66,6 +114,6 @@
   slider.addEventListener("focusin", stopAuto);
   slider.addEventListener("focusout", startAuto);
 
-  showSlide(0);
+  showSlide(0, "next", false);
   startAuto();
 })();
